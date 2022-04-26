80ca6270956f443b75c962f7a85377e82b65dd78
Michael <michaelcozzolino@outlook.it>
2022-04-26 13:01:32 +0200

    Fixed bug

--- database/migrations/2022_04_20_130304_create_sensors_table.php
+++ database/migrations/2022_04_20_130304_create_sensors_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('sensors', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 101);
             $table->double('lat')->nullable();
             $table->double('long')->nullable();
             $table->enum('status', ['connected', 'disconnected', 'connecting'])->default('connecting');
